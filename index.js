eb0d42323b0f8d92501e6e185f18fcf46917f362
ZUBAIR AHMAD <96621930+zbr-98@users.noreply.github.com>
2022-12-23 08:49:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var rN1=Math.floor(Math.random()*6)+1;
 var rN2=Math.floor(Math.random()*6)+1;
 
 
-var s1="/images/dice"+rN1+".png";
-var s2="/images/dice"+rN2+".png";
+var s1="images/dice"+rN1+".png";
+var s2="images/dice"+rN2+".png";
 
 document.querySelector(".img1").setAttribute("src",s1);
 document.querySelector(".img2").setAttribute("src",s2);
